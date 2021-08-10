871ed754c61879815e535ed6ea8e7437c993595b
Jeff <kabelj@gmail.com>
2021-08-09 19:38:00 -0700

    changes to support more contests

--- adif2cabrillo.py
+++ adif2cabrillo.py
@@ -1,9 +1,9 @@
 # Convert adif file to cabrillo for NAQP
-version = 0.1
+version = 0.11
 myCall = "AA6XA"
 myName = "JEFF"
 myState = "CA"
-
+mySection = "SCV"
 
 adifFlnm = input('Enter Adif filename to read: ')
 cabFlnm = input('Enter Cabrillo filename to write: ')
@@ -11,11 +11,12 @@ cabFlnm = input('Enter Cabrillo filename to write: ')
 fAdif = open(adifFlnm, 'r')
 fCab = open(cabFlnm, 'w')
 
+contest = input("Contest name: ")
 #write the fixed header
 fCab.write("START-OF-LOG: 3.0\n")
-fCab.write("CONTEST: NAQP-CW\n")
-fCab.write("LOCATION: SCV\n")
-fCab.write("CALLSIGN: AA6XA\n")
+fCab.write("CONTEST: "+contest+"\n")
+fCab.write("LOCATION: "+mySection+"\n")
+fCab.write("CALLSIGN: "+myCall+"\n")
 fCab.write("CATEGORY-OPERATOR: SINGLE-OP\n")
 fCab.write("CATEGORY-TRANSMITTER: ONE\n")
 fCab.write("CATEGORY-ASSISTED: NON-ASSISTED\n")
@@ -24,11 +25,11 @@ fCab.write("CATEGORY-POWER: LOW\n")
 fCab.write("CATEGORY-MODE: CW\n")
 fCab.write("CATEGORY-STATION: FIXED\n")
 fCab.write("CLAIMED-SCORE: \n")
-fCab.write("CREATED-BY: AA6XA's adif2cabrillo v0.1\n")
-fCab.write("NAME: Jeff Kabel\n")
+fCab.write("CREATED-BY: AA6XA's adif2cabrillo v"+version+"\n")
+fCab.write("NAME: "+myName+"\n")
 fCab.write("EMAIL: \n")
 fCab.write("ADDRESS: \n")
-fCab.write("OPERATORS: AA6XA\n")
+fCab.write("OPERATORS: "+myCall+"\n")
 fCab.write("SOAPBOX: \n")
 
 #loop over lines in Adif file
@@ -39,9 +40,12 @@ for line in adifEntries:
         idx = line.find("FREQ:")
         numChar = int(line[idx+5])
         freq = line[idx+7:idx+7+numChar]
-        #freq = freq*1000
         ii = freq.find(".")
-        freq = freq[0:ii]+freq[ii+1:ii+4] #+freq[ii]+freq[ii+4:numChar]
+        freq = freq[0:ii]+freq[ii+1:ii+4]
+
+        idx = line..find("MODE:")
+        numChar = int(line[idx+5])
+        mode = line[idx+7:idx+7+numChar]
 
         idx = line.find("QSO_DATE:")
         date = line[idx+11:idx+11+4]+"-"+line[idx+11+4:idx+11+6]+"-"+\
@@ -55,13 +59,19 @@ for line in adifEntries:
         numChar = int(line[idx+5])
         call = line[idx+7:idx+7+numChar]
 
+        # cabrillo rx exchange
         idx = line.find("SRX_STRING:")
         numChar = int(line[idx+11])
-        exch = line[idx+13:idx+13+numChar]
+        rxExch = line[idx+13:idx+13+numChar]
+
+        # cabrillo tx exchange
+        idx = line.find("STX_STRING:")
+        numChar = int(line[idx+11])
+        txExch = line[idx+13:idx+13+numChar]
 
         #write line in cabrillo file
-        fCab.write("QSO: "+freq+" CW "+date+" "+time+" "+myCall+" "+myName+\
-            " "+myState+" "+call+" "+exch+" \n")
+        fCab.write("QSO: "+freq+"  "+mode+" "+date+" "+time+" "+myCall+\
+            " "+txExch+" "+call+" "+rxExch+" \n")
 
 #write the footer, close files
 fCab.write("END-OF-LOG:\n")
